37a1acf34ed0140067598ee2503288ec90ae8df9
SpasPandev <sppan@abv.bg>
2024-11-14 19:26:47 +0200

    show the service providers with thymeleaf

--- src/main/java/com/example/servicehub/controller/tymeleafController/HomeControllerTL.java
+++ src/main/java/com/example/servicehub/controller/tymeleafController/HomeControllerTL.java
@@ -1,5 +1,8 @@
 package com.example.servicehub.controller.tymeleafController;
 
+import com.example.servicehub.service.ServiceProviderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,10 +11,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeControllerTL {
 
+    private final ServiceProviderService serviceProviderService;
+
+    public HomeControllerTL(ServiceProviderService serviceProviderService) {
+        this.serviceProviderService = serviceProviderService;
+    }
+
     @GetMapping("/home")
     public String showHomePage(Authentication authentication, Model model){
 
-        model.addAttribute("namm", authentication.getName());
+        ResponseEntity<?> all = serviceProviderService.findAll();
+
+        Object body = all.getBody();
+
+        if (all.getStatusCode() == HttpStatus.NOT_FOUND){
+
+            model.addAttribute("hasServiceProviders", false);
+            return "home";
+        }
+        model.addAttribute("hasServiceProviders", true);
+        model.addAttribute("allServiceProviders", all.getBody());
+
         return "home";
     }
 }
